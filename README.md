085146b1ffa63a4994e9e229dbd40d484befc1d7
Max <mkorobov2002@mail.ru>
2023-02-27 00:27:11 +0300

    Initialize repo

---
+++ README.md
@@ -0,0 +1,21 @@
+# dynamic-form
+
+## Build
+
+1. Install dependencies:
+
+`npm i`
+
+2. Run project:
+
+`npm start`
+
+3. Play with it here:
+
+[http://localhost:3000](http://localhost:3000)
+
+## Preview
+
+![Main page](docs/img/main.png)
+
+![Filled form](docs/img/filled-form.png)
---
+++ src/App.js
@@ -0,0 +1,98 @@
+import {
+  Button,
+  Card,
+  CardHeader,
+  Center,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+
+function App() {
+  const [teamName, setTeamName] = useState('');
+  const [members, setMembers] = useState([]);
+
+  const handleChangeTeamName = e => {
+    setTeamName(e.target.value);
+  };
+
+  const handleAddMember = () => {
+    setMembers([...members, { id: +new Date(), role: '' }]);
+  };
+
+  const handleChangeRole = (id, e) => {
+    setMembers(
+      members.map(member =>
+        member.id === id ? { ...member, role: e.target.value } : member
+      )
+    );
+  };
+
+  const handleDeleteMember = id => {
+    const updatedMembersList = members.filter(member => member.id !== id);
+    setMembers(updatedMembersList);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log({ teamName, members });
+    setTeamName('');
+    setMembers([]);
+  };
+
+  return (
+    <Center height='100vh'>
+      <Card maxWidth={500} flex={1} p={6} boxShadow='2xl'>
+        <form onSubmit={handleSubmit}>
+          <CardHeader p={0} mb={4} fontWeight={700} fontSize={32}>
+            <Center>Dynamic Form</Center>
+          </CardHeader>
+          <FormControl mb={4}>
+            <FormLabel>Team name</FormLabel>
+            <Input
+              type='text'
+              value={teamName}
+              onChange={handleChangeTeamName}
+              required
+            />
+          </FormControl>
+          <FormControl mb={4}>
+            <FormLabel>Members</FormLabel>
+            {members.map(member => (
+              <Flex key={member.id} gap={2} py={2}>
+                <Input
+                  type='text'
+                  placeholder='role'
+                  value={member.role}
+                  onChange={e => handleChangeRole(member.id, e)}
+                  required
+                />
+                <Button
+                  colorScheme='red'
+                  margin='0 auto'
+                  onClick={() => handleDeleteMember(member.id)}>
+                  x
+                </Button>
+              </Flex>
+            ))}
+          </FormControl>
+          <Flex p={4}>
+            <Button
+              colorScheme='blue'
+              margin='0 auto'
+              onClick={handleAddMember}>
+              +
+            </Button>
+          </Flex>
+          <Button type='submit' colorScheme='blue' width='full'>
+            Submit
+          </Button>
+        </form>
+      </Card>
+    </Center>
+  );
+}
+
+export default App;
